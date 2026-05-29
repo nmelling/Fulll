c2b82c689417e68b7a083cb9e2a1161157978f0d
nmelling <nicolas.mellinger.pro@gmail.com>
2026-05-29 11:15:23 +0200

    algo -- main function to display output with limit

--- Algo/index.ts
+++ Algo/index.ts
@@ -24,3 +24,15 @@ export function fizzbuzzLooper(max: number): FizzbuzzOutput[] {
   if (max < 1 || !Number.isSafeInteger(max)) return [];
   return new Array(max).fill(null).map((_, index) => fizzbuzz(index + 1));
 }
+
+function main() {
+  const max = Number(Bun.argv[2]);
+  if (Number.isNaN(max)) throw new Error("Please provide a limit as argument");
+  if (!Number.isSafeInteger(max) || max < 1) throw new Error("Please provide positive integer");
+
+  const res = fizzbuzzLooper(max);
+  return res;
+}
+
+// biome-ignore lint/suspicious/noConsole: For console user output
+console.log(main());
