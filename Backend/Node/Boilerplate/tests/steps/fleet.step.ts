63b92c5697755a048b759f53225e649c640af6f7
nmelling <nicolas.mellinger.pro@gmail.com>
2026-05-29 22:13:46 +0200

    base cucumber scenarii

---
+++ Backend/Node/Boilerplate/tests/steps/fleet.step.ts
@@ -0,0 +1,12 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given("my fleet", () => {});
+Given("I have registered this vehicle into my fleet", () => {});
+Given("the fleet of another user", () => {});
+Given("this vehicle has been registered into the other user's fleet", () => {});
+
+When("I register this vehicle into my fleet", () => {});
+When("I try to register this vehicle into my fleet", () => {});
+
+Then("this vehicle should be part of my vehicle fleet", () => {});
+Then("I should be informed this this vehicle has already been registered into my fleet", () => {});
