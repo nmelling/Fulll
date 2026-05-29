29ea4ae51e2bd97af05273b7c3d0255b74290b3c
nmelling <nicolas.mellinger.pro@gmail.com>
2026-05-30 00:14:46 +0200

    fleet -- cucumber tests + domain Fleet class

--- Backend/Node/Boilerplate/src/Domain/Fleet/fleet.ts
+++ Backend/Node/Boilerplate/src/Domain/Fleet/fleet.ts
@@ -1,3 +1,25 @@
-// Je relie un véhicule à une flotte
-// Possibilité d'ajouter/retirer un véhicule à ma flotte: Gestion de l'erreur si le véhicule appartient déjà à la meme flotte
-// Création d'une nouvelle flotte
+import { ERROR_MESSAGE as VEHICLE_ERROR_MESSAGE } from "../Vehicle/constants";
+import { ERROR_MESSAGE } from "./constants";
+
+export class Fleet {
+  private name: string | null;
+  private plateNumbers: Set<string>;
+
+  constructor(name: string) {
+    this.name = name;
+    this.plateNumbers = new Set();
+  }
+
+  registerVehicle(plateNumber: string) {
+    if (plateNumber.trim().length === 0) throw new Error(VEHICLE_ERROR_MESSAGE.INCORRECT_PLATE_NUMBER);
+    if (this.plateNumbers.has(plateNumber)) throw new Error(ERROR_MESSAGE.VEHICLE_ALREADY_REGISTERED);
+    this.plateNumbers.add(plateNumber);
+  }
+
+  get data() {
+    return {
+      name: this.name,
+      plateNumbers: this.plateNumbers,
+    };
+  }
+}
--- Backend/Node/Boilerplate/tests/steps/fleet.steps.ts
+++ Backend/Node/Boilerplate/tests/steps/fleet.steps.ts
@@ -1,14 +1,56 @@
-import { Given, Then, When } from "@cucumber/cucumber";
+import { expect } from "bun:test";
+import { Before, Given, setWorldConstructor, Then, When, World } from "@cucumber/cucumber";
+import { ERROR_MESSAGE } from "@/Domain/Fleet/constants";
+import { Fleet } from "@/Domain/Fleet/fleet";
 
-Given("my fleet", () => {
-  return {};
+export class WorldFleet extends World {
+  fleet!: Fleet;
+  anotherFleet!: Fleet; // Consider updating logic if more than two fleet are concerned. Keep it simple for now.
+  error?: Error;
+
+  async attempt(fn: () => void) {
+    try {
+      await fn();
+    } catch (e) {
+      this.error = e as Error;
+    }
+  }
+}
+
+setWorldConstructor(WorldFleet);
+
+const PLATE_NUMBER = "TEST-1";
+
+Before(function (this: WorldFleet) {
+  this.fleet = new Fleet("FLEET-1");
+  this.anotherFleet = new Fleet("FLEET-2");
 });
-Given("I have registered this vehicle into my fleet", () => {});
-Given("the fleet of another user", () => {});
-Given("this vehicle has been registered into the other user's fleet", () => {});
 
-When("I register this vehicle into my fleet", () => {});
-When("I try to register this vehicle into my fleet", () => {});
+Given("my fleet", function (this: WorldFleet) {
+  return this.fleet;
+});
+Given("I have registered this vehicle into my fleet", function (this: WorldFleet) {
+  if (!this.fleet.data.plateNumbers.has(PLATE_NUMBER)) this.fleet.registerVehicle(PLATE_NUMBER);
+});
+Given("the fleet of another user", function (this: WorldFleet) {
+  return this.anotherFleet;
+});
+Given("this vehicle has been registered into the other user's fleet", function (this: WorldFleet) {
+  this.attempt(() => this.anotherFleet.registerVehicle(PLATE_NUMBER));
+});
 
-Then("this vehicle should be part of my vehicle fleet", () => {});
-Then("I should be informed this this vehicle has already been registered into my fleet", () => {});
+When("I register this vehicle into my fleet", function (this: WorldFleet) {
+  this.attempt(() => this.fleet.registerVehicle(PLATE_NUMBER));
+});
+When("I try to register this vehicle into my fleet", function (this: WorldFleet) {
+  this.attempt(() => this.fleet.registerVehicle(PLATE_NUMBER));
+});
+
+Then("this vehicle should be part of my vehicle fleet", function (this: WorldFleet) {
+  expect(this.error).not.toBeDefined();
+  expect(this.fleet.data.plateNumbers.has(PLATE_NUMBER));
+});
+Then("I should be informed this this vehicle has already been registered into my fleet", function (this: WorldFleet) {
+  expect(this.error).toBeDefined();
+  expect(this.error?.message).toBe(ERROR_MESSAGE.VEHICLE_ALREADY_REGISTERED);
+});
