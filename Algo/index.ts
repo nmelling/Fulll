3bf54a1599daa0b589e8e9c9f01044610abcfe8f
nmelling <nicolas.mellinger.pro@gmail.com>
2026-05-29 10:51:35 +0200

    algo -- fizzbuzz function fonctionnal

--- Algo/index.ts
+++ Algo/index.ts
@@ -1,4 +1,4 @@
-import type { FizzbuzzOutput } from "./constants";
+import { BUZZ, FIZZ, FIZZBUZZ, type FizzbuzzOutput } from "./constants";
 
 export function isMultipleOf3(num: number): boolean {
   return !(num % 3);
@@ -9,5 +9,11 @@ export function isMultipleOf5(num: number): boolean {
 }
 
 export function fizzbuzz(num: number): number | FizzbuzzOutput {
+  if (num < 1) throw new Error("ONLY_POSITIVE_NUMBER_ARE_ALLOWED");
+  if (!Number.isSafeInteger(num)) throw new Error("ONLY_INTEGER_NUMBER_ARE_ALLOWED");
+
+  if (isMultipleOf3(num) && isMultipleOf5(num)) return FIZZBUZZ;
+  if (isMultipleOf3(num)) return FIZZ;
+  if (isMultipleOf5(num)) return BUZZ;
   return num;
 }
