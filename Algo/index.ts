070e2d1f4edab7feb8e4ef663abdce980cadc565
nmelling <nicolas.mellinger.pro@gmail.com>
2026-05-29 10:49:55 +0200

    algo -- create test + functions to define if multiple of 3 or 5

--- Algo/index.ts
+++ Algo/index.ts
@@ -1,5 +1,13 @@
 import type { FizzbuzzOutput } from "./constants";
 
+export function isMultipleOf3(num: number): boolean {
+  return !(num % 3);
+}
+
+export function isMultipleOf5(num: number): boolean {
+  return !(num % 5);
+}
+
 export function fizzbuzz(num: number): number | FizzbuzzOutput {
   return num;
 }
--- Algo/tests/algo.spec.ts
+++ Algo/tests/algo.spec.ts
@@ -1,33 +1,67 @@
 import { describe, expect, test } from "bun:test";
-import { fizzbuzz } from "..";
+import { fizzbuzz, isMultipleOf3, isMultipleOf5 } from "..";
 import { BUZZ, FIZZ, FIZZBUZZ } from "../constants";
+
+const NUM_FIZZBUZZ = 15;
+const NUM_FIZZ = 3;
+const NUM_BUZZ = 5;
+const NOT_MULTIPLE_NUM = 2;
+const NEGATIVE_NUM = -1;
+const FLOAT_NUM = 3.14;
+const ZERO = 0;
+
+describe("isMultipleOf3 behaviour", () => {
+  test("Returns true if provided number is multiple of 3", () => {
+    expect(isMultipleOf3(NUM_FIZZ)).toBe(true);
+    expect(isMultipleOf3(NUM_FIZZBUZZ)).toBe(true);
+    expect(isMultipleOf3(ZERO)).toBe(true);
+    expect(isMultipleOf3(NUM_FIZZBUZZ * -1)).toBe(true);
+  });
+  test("Returns false if provided number is not a multiple of 3", () => {
+    expect(isMultipleOf3(NUM_BUZZ)).toBe(false);
+    expect(isMultipleOf3(NOT_MULTIPLE_NUM)).toBe(false);
+    expect(isMultipleOf3(FLOAT_NUM)).toBe(false);
+  });
+});
+describe("isMultipleOf5 behaviour", () => {
+  test("Returns true if provided number is multiple of 3", () => {
+    expect(isMultipleOf5(NUM_BUZZ)).toBe(true);
+    expect(isMultipleOf5(NUM_FIZZBUZZ)).toBe(true);
+    expect(isMultipleOf5(ZERO)).toBe(true);
+    expect(isMultipleOf5(NUM_FIZZBUZZ * -1)).toBe(true);
+  });
+  test("Returns false if provided number is not a multiple of 3", () => {
+    expect(isMultipleOf5(NUM_FIZZ)).toBe(false);
+    expect(isMultipleOf5(NOT_MULTIPLE_NUM)).toBe(false);
+    expect(isMultipleOf5(FLOAT_NUM)).toBe(false);
+  });
+});
 
 describe("FizzBuzz basic behaviour", () => {
   test("If number can be divided by 3 AND 5: display FizzBuzz", () => {
-    const res = fizzbuzz(15);
+    const res = fizzbuzz(NUM_FIZZBUZZ);
     expect(res).toBe(FIZZBUZZ);
   });
   test("If number can be divided by 3 AND NOT by 5: display Fizz", () => {
-    const res = fizzbuzz(3);
+    const res = fizzbuzz(NUM_FIZZ);
     expect(res).toBe(FIZZ);
   });
   test("If number can be divided by 5 AND NOT by 3: display Buzz", () => {
-    const res = fizzbuzz(5);
+    const res = fizzbuzz(NUM_BUZZ);
     expect(res).toBe(BUZZ);
   });
   test("Else: display the number", () => {
-    const NUM = 2;
-    const res = fizzbuzz(NUM);
-    expect(res).toBe(NUM);
+    const res = fizzbuzz(NOT_MULTIPLE_NUM);
+    expect(res).toBe(NOT_MULTIPLE_NUM);
   });
 });
 
 describe("FizzBuzz incorrect entry", () => {
   test("Any number below 1 not allowed", () => {
-    expect(() => fizzbuzz(0)).toThrow();
-    expect(() => fizzbuzz(-1)).toThrow();
+    expect(() => fizzbuzz(ZERO)).toThrow();
+    expect(() => fizzbuzz(NEGATIVE_NUM)).toThrow();
   });
   test("Float number not allowed", () => {
-    expect(() => fizzbuzz(3.14)).toThrow();
+    expect(() => fizzbuzz(FLOAT_NUM)).toThrow();
   });
 });
